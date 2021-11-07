76c97b9fa45924b4ffecc0b89765b5c497b99a88
singham5497 <93856863+singham5497@users.noreply.github.com>
2021-11-07 07:15:28 +0000

    Add files via upload

---
+++ pengaturan.php
@@ -0,0 +1,5 @@
+<?php
+
+$useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36";
+
+$cookie = "ci_session=8c53oj8365jja9n2e55aj0uj0hnbm8qs;_ym_uid=1636268156695841450;_ym_d=1636268156;_ym_isad=2;csrf_cookie_name=981cedd28c8d57fe2e73bb3e0313acdf";
